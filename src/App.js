9297b7336ea56a70c9520c750ed5b396cd391653
muhammad <m7akem@github.com>
2020-01-08 21:55:18 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./assets/css/main.css";
+import "./assets/css/icons.css";
+import "./assets/css/animate.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="home-page">
+        <div className="background-image"></div>
+        <div className="right">
+          <div className="content"></div>
+          <div className="content-border animated fadeInRight"></div>
+          <div className="content-border-1 animated fadeInRight"></div>
+          <div className="logo"> </div>
+          <div className="description">
+            <p>
+              هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد
+              هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو
+              العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها
+              التطبيق.
+            </p>
+          </div>
+          <div className="sectors">
+            <ul>
+              <li>
+                <a href="#">
+                  <div className="sector" style={{animationDelay: "0.5s"}}>
+                    <div className="sector-icon">
+                      <i className="estishraf-icon icon-business-whiteboard-graph"></i>
+                    </div>
+                    <span>القطاع الأول</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="sector" style={{animationDelay: "0.6s"}}>
+                    <div className="sector-icon">
+                      <i className="estishraf-icon fa-money"></i>
+                    </div>
+                    <span>القطاع الثاني</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div className="sector" style={{animationDelay: "0.7s"}}>
+                    <div className="sector-icon">
+                      <i className="estishraf-icon icon-money-stack"></i>
+                    </div>
+                    <span>القطاع الثالث</span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <footer>
+            <ul>
+              <li>
+                <i className="estishraf-icon icon-business-graph-bar-increase"></i>
+                <div className="divider"></div>
+                <span>تعزيز الإيرادات الغير نفطية</span>
+              </li>
+              <li>
+                <i className="estishraf-icon icon-computer-share"></i>
+                <div className="divider"></div>
+                <span>أرقام وتطوير</span>
+              </li>
+              <li>
+                <i className="estishraf-icon icon-business-whiteboard-graph"></i>
+                <div className="divider"></div>
+                <span>تحليلات ومؤشرات</span>
+              </li>
+            </ul>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
